dce73663cdd08dd751e82fd296bf76b5221e232f
Aarti Kalamangalam <aartikalamangalam@gmail.com>
2025-04-03 14:41:03 -0400

    change api endpoint to be in .env file and add readme instructions

--- api/config/Database.php
+++ api/config/Database.php
@@ -3,7 +3,7 @@ class Database {
     private $host = "localhost";
     private $db_name = "auth_db";
     private $username = "root";
-    private $password = "root";
+    private $password = "";
     private $conn;
 
     // Database connection
